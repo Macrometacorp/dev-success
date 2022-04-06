ae1f9500b478c2505f55cbf5fa8cdaa2ba87f231
Ljubisav <91734363+Luba89@users.noreply.github.com>
2022-04-07 01:55:19 +0200

    add

---
+++ coloneCollection/app.js
@@ -0,0 +1,151 @@
+const jsc8 = require("jsc8");
+
+const collectionName = "nameOfCollection" //INPUT name of collection that you want to clone
+const copyFabric = "_system"; //Collection location fabric
+const pasteFabric = "nameOfFabric";   // Collection destination fabric
+const url = "https://gdn.paas.macrometa.io/";  //Federation URL
+const apiKey = ""
+const client1 = new jsc8({
+  url,
+  apiKey,
+  fabricName: copyFabric,
+});
+const client2 = new jsc8({
+  url,
+  apiKey,
+  fabricName: pasteFabric,
+});
+
+const cloneCollections = async function() {
+    const i = await client1.getCollection(collectionName)
+    try {
+        if (i.collectionModel === "DOC") {
+          if (i.type === 2) {
+            await client2.createCollection(i.name, { stream: i.hasStream ? true : false });
+          } else {
+            await client2.createCollection(i.name, { stream: i.hasStream ? true : false }, { isEdge: true });
+          }
+        } else if (i.collectionModel === "KV") {
+          const result = await client2.createKVCollection(i.name, {
+            expiration: true,
+            stream: i.hasStream ? true : false,
+          });
+        } else if (i.collectionModel === "DYNAMO") {
+          console.log(
+            `"${i.name}" DYNAMO collection is not cloned, there is no driver support for this type of collection`
+          );
+        } else {
+          console.log("I dont know this type");
+        }
+      
+      console.log("Collection cloning process id DONE");
+    } catch (e) {
+      console.log(
+        "Somthing went wrong with collection cloning requst, please make sure that destination fabric dont have collection with same name"
+      );
+      console.log(e.response.body);
+    }
+  };
+  const cloneData = async function() {
+    let data = [];
+    const batchSize = 1000;
+    let obj = new Object();
+    const i = await client1.getCollection(collectionName)
+  
+    try {
+      console.log("Loading data is started!");
+        //Determining collection size and number of times that we are going to call query
+        let name = i.name;
+        const { count } = await client1.collection(name).count();
+        const num1 = Math.ceil(count / batchSize);
+  
+        for (a = 0; a < num1; a++) {
+          //We change the offset for each iteration
+          let offset = a * batchSize;
+          query = `FOR doc IN ${name} limit ${offset}, ${batchSize} return doc`;
+          const cursor = await client1.query(query, {}, { batchSize: batchSize });
+          data.push.apply(data, cursor._result);
+          console.log(`Loading ${a+1} of ${num1}`);
+
+        }
+        obj[name] = data;
+        data = [];
+      console.log("Data is loaded");
+    } catch (e) {
+      console.log("Data cant be loaded");
+      console.log(e.response.body);
+    }
+    try {
+      console.log("Cloning data is started")
+        let limit = 10000
+        let bat =10000
+        let a=0
+        let insert = obj[i.name]
+        let count = insert.length
+        const num2 = Math.ceil(count / limit);
+        let coll = i.name
+        for (c = 0; c < num2; c++) {    
+          let b =insert.slice(a,(bat))
+          await client2.importDocuments(coll, b,true, '_key', true);
+          a=a+limit
+          bat=bat+limit
+          console.log(`Cloning ${c+1} of ${num2}`);
+
+        
+    }
+      console.log("Data is cloned to new fabric");
+    } catch (e) {
+      console.log("There is error in data cloning process");
+      console.log(e);
+    }
+  };
+
+const cloneIndexes = async function() {
+    const i = await client1.getCollection(collectionName)
+    let arr = [];
+    const collectionIndexes = await client1.getCollectionIndexes(i.name);
+      for (let i of collectionIndexes) {
+        if (i.type !== "primary" && i.type !== "edge" && i.fields[0] !== "expireAt") {
+          arr.push(i);
+        }
+      }
+    
+    //console.log(arr);
+    try {
+      for (let i of arr) {
+        if (i.type === "fulltext") {
+          let name = i.id.split("/")[0];
+          await client2.addFullTextIndex(name, i.fields);
+        } else if (i.type === "geo") {
+          let name = i.id.split("/")[0];
+          await client2.addGeoIndex(name, i.fields);
+        } else if (i.type === "ttl") {
+          let name = i.id.split("/")[0];
+          await client2.addTtlIndex(name, i.fields, i.expireAfter);
+        } else if (i.type === "persistent") {
+          let name = i.id.split("/")[0];
+          await client2.addPersistentIndex(name, i.fields, {
+            unique: i.unique ? true : false,
+            sparse: i.sparse ? true : false,
+            deduplicate: i.deduplicate ? true : false,
+          });
+        }
+      }
+      console.log("Index cloning process is DONE!");
+    } catch (e) {
+      console.log("Something went wrong with index cloning process");
+      console.log(e.response.body);
+    }
+  };
+  
+  const runInSeries = async () => {
+    const list = [
+      cloneCollections,
+      cloneIndexes,
+      cloneData,
+    ];
+    for (const fn of list) {
+      await fn(); // call function to get returned Promise
+    }
+  };
+  runInSeries();
